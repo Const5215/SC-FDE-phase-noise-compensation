4744d4574aca872df01bf064efe2306fb9a77d49
Const5215 <const5215@outlook.com>
2018-05-13 17:36:46 +0800

    phaseNoisePSDadded

--- Untitled.m
+++ Untitled.m
@@ -3,7 +3,7 @@
 global sim_consts;
 sim_consts.SampFreq = 1.76*1e9;
 dbc_level = -80;
-n_samples = 1568;
+n_samples = 156800;
 corner_freq = 1e6;
 phase_noise_floor = -120;
 % Variance of top flat noisd
@@ -31,6 +31,11 @@ noise_floor = sqrt(noise_floor_var)*randn(1, n_samples);
 integrator_input = filter(b_hpf, a_hpf, colored_noise);
 phase_noise      = filter(b_intg, a_intg, integrator_input) + noise_floor;
 
-plot(phase_noise);
+% plot(phase_noise);
 % tmp = xcorr(phase_noise,'coeff');
 % plot(tmp(n_samples:n_samples+512));
+
+[Pxx,f] = pwelch(phase_noise,hanning(1000),0,n_samples,sim_consts.SampFreq);
+semilogx(f,10*log10(Pxx));
+xlabel('f');
+ylabel('dBc/Hz');
